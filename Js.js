26d252839281036cd34fdb17e5e2e42f4d85010f
enrimatiaspedraz <matiaspedraz@hotmail.com>
2023-06-05 01:37:22 -0300

    5junio

--- Js.js
+++ Js.js
@@ -11,44 +11,46 @@ const total = document.getElementById("total")
 
 const precio = 200
 
-const categorias = {
-  a: {porcentaje: 80, value:"a"},
-  b: {porcentaje: 50, value:"b"},
-  c: {porcentaje: 15, value:"c"}
+const categories = {
+  a: { porcentaje: 80, value: '0' },
+  b: { porcentaje: 50, value: '1' },
+  c: { porcentaje: 15, value: '2' }
 }
 
 let cantidad = null
-let categoria = null
+let category = null
 let totalAPagar = null
 
 
-const textoTotal = "Monto a Pagar $: "
+const textoTotal = "Total a Pagar: $ "
 
 
  
 const preciofinal = () => {
 
-cantidad = 2
-
   if (!cantidad || !precio) return
 
   const APagar = precio * cantidad
-  const descuento = (APagar/100) *  10
+  const descuento = (APagar/100) * categories[category].porcentaje
   totalAPagar = APagar - descuento
   montoAPagar.innerText = textoTotal + totalAPagar  
 }
 
 
-  const borrarcampos = () => {
+  const borrarcampos = (e) => {
     for (let input of inputs)
     input.value="";
     select.value="none"
+    montoAPagar.innerText = "Total a Pagar: $"
   }
+
 
   borrar.addEventListener("click", borrarcampos)
 
-  const enviar = () => {
-    console.log("jjj")
+
+  // esto hay que cambiarlo
+  const enviar = (e) => {
+    preciofinal()
   }
 
   total.addEventListener("click", enviar)
@@ -59,18 +61,29 @@ cantidad = 2
   
 
   const setCategory = (e) => {
-    const opcion = e.target.value
-    console.log(opcion);
+    const option = e.target.value
+    if (option === 'none') {
+      borrarcampos()
+      return
+       }
 
+    category = option
+  
   }
+  console.log(category);
 
-  document.getElementById("categoria").addEventListener("change", setCategory)
+  document.getElementById("category").addEventListener("change", setCategory)
 
 
 
   const setCantidad = (e) => {
-    const quantity = e.target.value
-    console.log(quantity);
+    const value = e.target.value
+    if (value < 0){
+    e.target.value = 0
+    totalAPagar = null
+    return}
+    cantidad = value
+    //preciofinal()
     
   }
 
